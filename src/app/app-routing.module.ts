62b967d42a19349b093e9c2c56f2129b7c0fcc13
Constant Crochet <49516449+ConstantC@users.noreply.github.com>
2022-05-14 15:44:18 +0200

    404
    Finishing the 404 page
    
    Expanding the translate variables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,9 @@ import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page-not-found.component', pathMatch: 'full' },
   { path: 'page-not-found.component', component: PageNotFoundComponent, canActivate : [AuthGuard] },
   { path: 'header', component: HeaderComponent, canActivate : [AuthGuard] },
-  { path: '**', component: PageNotFoundComponent,
-  loadChildren: () => import('./app.module').then(m => m.AppModule)}
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
