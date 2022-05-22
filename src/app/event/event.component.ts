7d0966d0b51b9946752580e55a852b436e51337c
Constant Crochet <49516449+ConstantC@users.noreply.github.com>
2022-05-22 13:03:10 +0200

    removed materialize
    Removed materialize and finished events pages

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import {DatePipe} from '@angular/common';
 import { lastValueFrom } from 'rxjs';
+import { UsersService } from '../utility/users.service';
+import { EventsService } from '../utility/events.service';
 
 @Component({
   selector: 'app-event',
@@ -11,19 +13,24 @@ import { lastValueFrom } from 'rxjs';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
-  eventUrl: string = 'https://event-service.cubetech-app.fr'
   startDate = new Date('2022-05-18T12:30'); 
   endDate = new Date('2022-05-18T14:30');
   profilePictureUrl = '../../assets/logo-whiteback-round.png';
-  profileName = 'profile name';
+  profileName = '';
+  user: any;
   id: string|null = '';
   lang!: string;
   eventById: any;
   events: any;
+  eventsNumber: number |undefined;
+  eventsUser: any;
+  eventsUserNumber: number |undefined;
   participantNumber: number | undefined;
   subscription: any
+  users: any[] | undefined;
 
-  constructor(private translateService: TranslateService, public router: Router, private route: ActivatedRoute, private http: HttpClient, public datePipe: DatePipe) { 
+  constructor(private translateService: TranslateService, public router: Router, private route: ActivatedRoute, 
+              private eventsService: EventsService, public datePipe: DatePipe, private usersService: UsersService) { 
     this.eventById = null;
     this.subscription = this.router.events.subscribe((ev) => {
       if (ev instanceof NavigationEnd) {
@@ -35,42 +42,54 @@ export class EventComponent implements OnInit {
   }
 
   async ngOnInit(): Promise<void> {
+    await this.usersService.getUser().then(user => {
+      this.user = user;
+    });
+    this.profileName = this.user.lastName + ' ' + this.user.firstName;
     this.lang = this.translateService.getBrowserLang();
+    await this.eventsService.getEventByUserId(this.user.userId).then(eventsUser => {
+      this.eventsUser = eventsUser;
+    });
+    this.eventsUserNumber = this.eventsUser.length
+
+    if(this.user.profilePicture !== null){
+      this.profilePictureUrl = this.user.profilePicture;
+    }
+
     if(this.router.url === '/event'){    
-      this.getAllEvents()
+      await this.eventsService.getAllEvents().then(events =>
+        this.events = events);
+      this.eventsNumber = this.events.length
     }
     else if(this.id !== null){
-      this.eventById = await this.getEventById(this.id)
+      this.eventById = await this.eventsService.getEventById(this.id)
       this.participantNumber = this.eventById.participantsId.length
+      await this.usersService.getUserList(this.eventById.participantsId).then(users => {
+        this.users = users;
+      })
     }
-    
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  getAllEvents(): void {
-    this.http.get(this.eventUrl + '/event/events').subscribe(apiEvents => {
-        this.events = apiEvents;
-        console.log(this.events);
-    });
+
+
+  async signUp(){
+    await this.eventsService.addParticipant(this.user.userId, this.id);
+    location.reload();
   }
 
-  async getEventById(eventId: string|null): Promise<any> {
-    let event= await lastValueFrom(this.http.get(this.eventUrl + '/event/findEvent/' + eventId))
-    return event
-  }
-
-  signUp(){
-
-  }
-
-  signOut(){
-
+  async signOut(){
+    console.log('test1')
+    await this.eventsService.deleteParticipant(this.user.userId, this.id);
+    console.log('test2')
+    location.reload();
   }
 
   isSignedUp(){
-    return false;
+    if(this.id !== null){return this.eventById.participantsId.includes(this.user.userId)}
+    else return false
   }
 }
